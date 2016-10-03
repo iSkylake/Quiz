11f6d25ae82afad33b7ed2a2d156b67704ce2e91
Llorch <SkylakeBeta@gmail.com>
2016-10-02 18:16:34 -0700

    Comment logout flash message at index route

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ router.post("/login", passport.authenticate("local",
 
 router.get("/logout", function(req, res){
 	req.logout();
-	req.flash("error", "Successfully logged out");
+	// req.flash("success", "Successfully logged out");
 	res.redirect("/");
 });
 
